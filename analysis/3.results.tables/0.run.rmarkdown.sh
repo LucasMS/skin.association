dcfff37342e032d837874d814dd4b85ce2a3412a
Lucas <lucmoitinho@gmail.com>
2020-10-28 11:55:08 +0100

    Update summary figures

--- analysis/3.results.tables/0.run.rmarkdown.sh
+++ analysis/3.results.tables/0.run.rmarkdown.sh
@@ -1,17 +1,11 @@
 DIROUT='/home/lsilva/IKMB/projects/skin.associations/results/analysis/3.results.tables'
-FILENAME='asv'
-###
-INPUT=${FILENAME}'.Rmd'
-OUTPUT=${FILENAME}'.html'
-FIGPATH=${DIROUT}'/'${FILENAME}'/'
-mkdir "$DIROUT"
-R -e "Sys.setenv(RSTUDIO_PANDOC='/usr/lib/rstudio/bin/pandoc');rmarkdown::render('${INPUT}', output_file='${DIROUT}/${OUTPUT}', params = list(FIGPATH='${FIGPATH}', d.out = '${DIROUT}'))"
 
-
-FILENAME='beta'
-###
-INPUT=${FILENAME}'.Rmd'
-OUTPUT=${FILENAME}'.html'
-FIGPATH=${DIROUT}'/'${FILENAME}'/'
-mkdir "$DIROUT"
-R -e "Sys.setenv(RSTUDIO_PANDOC='/usr/lib/rstudio/bin/pandoc');rmarkdown::render('${INPUT}', output_file='${DIROUT}/${OUTPUT}', params = list(FIGPATH='${FIGPATH}', d.out = '${DIROUT}'))"
+for i in beta asv make.plot
+do
+ FILENAME=${i}
+ INPUT=${FILENAME}'.Rmd'
+ OUTPUT=${FILENAME}'.html'
+ FIGPATH=${DIROUT}'/'${FILENAME}'/'
+ mkdir "$DIROUT"
+ R -e "Sys.setenv(RSTUDIO_PANDOC='/usr/lib/rstudio/bin/pandoc');rmarkdown::render('${INPUT}', output_file='${DIROUT}/${OUTPUT}', params = list(FIGPATH='${FIGPATH}', d.out = '${DIROUT}'))"
+done
