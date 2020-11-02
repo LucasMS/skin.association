c9c976f01ffd951bc4f8e12a5f9485d011fc15f6
Lucas <lucmoitinho@gmail.com>
2020-11-02 12:48:38 +0100

    Edit shared
    Add script to make sup table

--- analysis/1.combine.microbiome/0.calculate.shared/0.run.rmarkdown.sh
+++ analysis/1.combine.microbiome/0.calculate.shared/0.run.rmarkdown.sh
@@ -1,8 +1,8 @@
-FILENAME='plot.sharedness'
-DIROUT='/home/lsilva/IKMB/projects/skin.associations/results/analysis/1.combine.microbiome/0.sharedness'
+FILENAME='get.table'
+DIROUT='/home/lsilva/IKMB/projects/skin.associations/results/analysis/1.combine.microbiome/0.calculate.shared'
 ###
 INPUT=${FILENAME}'.Rmd'
 OUTPUT=${FILENAME}'.html'
 FIGPATH=${DIROUT}'/'${FILENAME}'/'
-mkdir "$DIROUT" -p
+mkdir "$DIROUT"
 R -e "Sys.setenv(RSTUDIO_PANDOC='/usr/lib/rstudio/bin/pandoc');rmarkdown::render('${INPUT}', output_file='${DIROUT}/${OUTPUT}', params = list(FIGPATH='${FIGPATH}', d.out = '${DIROUT}'))"
