895dee190db69e795deb5917fd16ee6cc2d9c2bf
Lucas <lucmoitinho@gmail.com>
2021-01-26 13:09:17 +0100

    improve visualization

--- analysis/1.combine.microbiome/3.plot/0.run.rmarkdown.sh
+++ analysis/1.combine.microbiome/3.plot/0.run.rmarkdown.sh
@@ -8,6 +8,8 @@ mkdir "$DIROUT"
 R -e "Sys.setenv(RSTUDIO_PANDOC='/usr/lib/rstudio/bin/pandoc');rmarkdown::render('${INPUT}', output_file='${DIROUT}/${OUTPUT}', params = list(FIGPATH='${FIGPATH}', d.out = '${DIROUT}'))"
 
 FILENAME='make.panel.fig'
+DIROUT='/home/lsilva/IKMB/projects/skin.associations/results/analysis/1.combine.microbiome/3.plot'
+
 ###
 INPUT=${FILENAME}'.Rmd'
 OUTPUT=${FILENAME}'.html'
